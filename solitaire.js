267f2cc3ab71f8aeb487777686d5f292a700f8c5
Ari Stoler <astoler428@gmail.com>
2022-12-31 13:06:25 -0600

    added in sliding and translating for all moves

--- solitaire.js
+++ solitaire.js
@@ -1,8 +1,4 @@
-//clean up code by having a move function which moves a card from one stack to another
-//previously verified it can be moved and then just execute - genral for all cards behind
-//change card to have a data value of location set to deck, discard, stack#, pile# rather than looking for it
 const suits = ["♣", "♦", "♥", "♠"];
-const colors = ["red", "black"];
 const ranks = [
   "A",
   "2",
@@ -19,6 +15,8 @@ const ranks = [
   "K",
 ];
 
+const CARD_OVERLAP = 35;
+let z = 10;
 let gameOver;
 let deck;
 let fullDeckInIDorder;
@@ -34,7 +32,7 @@ const discardPile = document.getElementById("discard-pile");
 const restartBtn = document.getElementById("restart");
 const winLabel = document.getElementById("win-label");
 
-deckPile.addEventListener("click", flipCard);
+deckPile.addEventListener("click", drawFromDeck);
 restartBtn.addEventListener("click", restartGame);
 
 class Card {
@@ -44,6 +42,8 @@ class Card {
     this.location = location;
     this.id = id;
     this.faceUp = false;
+    this.justFlipped = false;
+    this.justMoved = null;
   }
 
   onTop() {
@@ -57,7 +57,7 @@ class Card {
   getHTML() {
     let cardDiv = document.createElement("div");
     cardDiv.id = this.id;
-    cardDiv.addEventListener("dblclick", moveCard);
+    cardDiv.addEventListener("dblclick", tryToMoveCard);
 
     if (this.faceUp == true) {
       cardDiv.innerHTML = this.rank;
@@ -98,7 +98,10 @@ function startGame() {
 }
 
 function restartGame() {
+  //bug where the deck isn't getting displayed beacuse I need to reappend child of deck display
   winLabel.innerHTML = "";
+  deckPile.innerHTML = "";
+  deckPile.appendChild(deckPileDisplay);
   startGame();
 }
 
@@ -144,40 +147,101 @@ function dealGame() {
 
 //flips top card off deck and puts it in discard pile
 //takes cards off front of deck array and adds to back of discard array so when deck is reset to discards, it's in order
-function flipCard() {
+function drawFromDeck() {
   if (deck.length == 0) {
     deck = discards;
     discards = [];
+    if (deck.length != 0) animateDeckReset();
   } else {
     let card = deck.shift();
     card.faceUp = true;
     discards.push(card);
     card.location = discards;
+    animateDeckFlip();
   }
-  render();
+  //render();
 }
 
-function moveCard(event) {
+function tryToMoveCard(event) {
   if (gameOver) return;
   let card = fullDeckInIDorder[event.target.id];
-  console.log(card);
 
   //conditionally checking tryToMoveToStack only if tryToPlay is false
-  !tryToPlay(card) && tryToMoveToStack(card);
-  tryToTurnOver(card);
-  render();
-  checkWin();
+  if (
+    tryToPlay(card, event.target) ||
+    tryToMoveToStack(card, event.target) ||
+    tryToTurnOver(card)
+  ) {
+    render();
+    checkWin();
+  }
 }
 
-function move(card, to) {
+function tryToPlay(card, containerDiv) {
+  //first check that it's a faceup card and on top of it's stack, otherwise can't be played
+  if (!card.faceUp || !card.onTop()) return;
+
+  let playStack = playStacks[suits.indexOf(card.suit)];
+
+  if (playStack.length == 0) {
+    //special case
+    if (card.rank == "A") {
+      move(card, playStack, containerDiv);
+      return true;
+    }
+  } else {
+    let stackCard = playStack[playStack.length - 1];
+    let cardRankSpot = ranks.indexOf(card.rank);
+    let stackRankSpot = ranks.indexOf(stackCard.rank);
+    if (cardRankSpot == stackRankSpot + 1) {
+      move(card, playStack, containerDiv);
+      return true;
+    }
+  }
+  return false;
+}
+
+function tryToMoveToStack(card, containerDiv) {
+  if (!card.faceUp) return false;
+
+  for (let stack of pileStacks) {
+    if (stack.length == 0 && card.rank == "K") {
+      move(card, stack, containerDiv);
+      return true;
+    } else if (stack.length != 0) {
+      let cardInStack = stack[stack.length - 1];
+      if (
+        cardInStack.faceUp &&
+        ranks.indexOf(card.rank) == ranks.indexOf(cardInStack.rank) - 1 &&
+        !sameColor(card, cardInStack)
+      ) {
+        move(card, stack, containerDiv);
+        return true;
+      }
+    }
+  }
+}
+
+function move(card, to, containerDiv) {
   let stackContainingCard = card.location;
 
   let cardPosition = stackContainingCard.indexOf(card);
   let toMove = stackContainingCard.slice(cardPosition);
-  for (let card of toMove) {
+  for (index = 0; index < toMove.length; index++) {
+    let card = toMove[index];
     to.push(card);
     card.location = to;
+    let rect = containerDiv.getBoundingClientRect();
+    card.justMoved = {
+      x: rect.left,
+      y: rect.top + index * CARD_OVERLAP,
+    };
   }
+  // for (let card of toMove) {
+  //   to.push(card);
+  //   card.location = to;
+  //   card.justMoved = true;
+  // }
   for (
     let index = stackContainingCard.length - 1;
     index >= cardPosition;
@@ -187,53 +251,12 @@ function move(card, to) {
   }
 }
 
-function tryToPlay(card) {
-  //first check that it's a faceup card and on top of it's stack, otherwise can't be played
-  if (!card.faceUp || !card.onTop()) return;
-
-  let playStack = playStacks[suits.indexOf(card.suit)];
-
-  if (playStack.length == 0) {
-    //special case
-    if (card.rank == "A") {
-      move(card, playStack);
-      return true;
-    }
-  } else {
-    let stackCard = playStack[playStack.length - 1];
-    let cardRankSpot = ranks.indexOf(card.rank);
-    let stackRankSpot = ranks.indexOf(stackCard.rank);
-    if (cardRankSpot == stackRankSpot + 1) {
-      move(card, playStack);
-      return true;
-    }
+function tryToTurnOver(card) {
+  if (!card.faceUp && card.onTop()) {
+    card.justFlipped = true;
+    return true;
   }
   return false;
-}
-
-function tryToMoveToStack(card) {
-  if (!card.faceUp) return;
-
-  for (let stack of pileStacks) {
-    if (stack.length == 0 && card.rank == "K") {
-      move(card, stack);
-      return;
-    } else if (stack.length != 0) {
-      let cardInStack = stack[stack.length - 1];
-      if (
-        cardInStack.faceUp &&
-        ranks.indexOf(card.rank) == ranks.indexOf(cardInStack.rank) - 1 &&
-        !sameColor(card, cardInStack)
-      ) {
-        move(card, stack);
-        return;
-      }
-    }
-  }
-}
-
-function tryToTurnOver(card) {
-  if (!card.faceUp && card.onTop()) card.faceUp = true;
 }
 
 function sameColor(card1, card2) {
@@ -253,43 +276,151 @@ function checkWin() {
   }
 }
 function render() {
-  renderDeckAndDiscard();
   renderPlayStacks();
   renderPileStacks();
+  renderDiscard();
 }
 
 function renderPileStacks() {
   for (let stackNum = 0; stackNum < pileStacks.length; stackNum++) {
     stackContainers[stackNum].innerHTML = "";
     for (let cardNum = 0; cardNum < pileStacks[stackNum].length; cardNum++) {
-      let cardHTML = pileStacks[stackNum][cardNum].getHTML();
-      cardHTML.style.top = 35 * cardNum + "px";
-      stackContainers[stackNum].appendChild(cardHTML);
+      let card = pileStacks[stackNum][cardNum];
+      let cardHTML = card.getHTML();
+      let endTop = CARD_OVERLAP * cardNum;
+      if (card.justMoved != null)
+        animateCardMove(card, cardHTML, stackContainers[stackNum], endTop);
+      else {
+        cardHTML.style.top = endTop + "px";
+        stackContainers[stackNum].appendChild(cardHTML);
+        if (card.justFlipped) animateCardFlip(card, cardHTML);
+      }
     }
   }
-}
-
-function renderDeckAndDiscard() {
-  if (deck.length == 0) {
-    deckPile.innerHTML = "Click";
-  }
-  if (deck.length > 0 && deckPile.querySelector("deck-pile-display") == null)
-    deckPile.appendChild(deckPileDisplay);
-  if (discards.length == 0) discardPile.innerHTML = "";
-  else discardPile.appendChild(discards[discards.length - 1].getHTML());
 }
 
 function renderPlayStacks() {
   for (let i = 0; i < playStacks.length; i++) {
     let stack = playStacks[i];
-    if (stack.length != 0)
-      playContainers[i].appendChild(stack[stack.length - 1].getHTML());
-    else {
+    if (stack.length != 0) {
+      let card = stack[stack.length - 1];
+      let cardHTML = card.getHTML();
+      if (card.justMoved != null)
+        animateCardMove(card, cardHTML, playContainers[i], 0);
+      else playContainers[i].appendChild(stack[stack.length - 1].getHTML());
+    } else {
       playContainers[i].innerHTML = suits[i];
     }
   }
 }
 
-//comment the code - as this will be a project and I plan to keep improving it
+function animateCardMove(card, cardHTML, containerDiv, endTop) {
+  let newLocation = containerDiv.getBoundingClientRect();
+  let deltaX = newLocation.left - card.justMoved.x;
+  let deltaY = newLocation.top - card.justMoved.y;
+  card.justMoved = null;
 
-//improvements: sound, animation, drag / drop, undo??? etc.
+  containerDiv.style.zIndex = z++;
+
+  let currentLeft = -1 * deltaX;
+  let currentTop = -1 * deltaY;
+
+  cardHTML.style.left = currentLeft + "px";
+  cardHTML.style.top = currentTop + "px";
+
+  containerDiv.appendChild(cardHTML);
+
+  let numberOfSlides = 50;
+
+  let leftSlide = deltaX / numberOfSlides;
+  let topSlide = (endTop + deltaY) / numberOfSlides;
+
+  let intervalID = setInterval(slide, 1);
+  let steps = 0;
+  function slide() {
+    if (steps++ == numberOfSlides) clearInterval(intervalID);
+    else {
+      currentLeft += leftSlide;
+      currentTop += topSlide;
+      cardHTML.style.left = currentLeft + "px";
+      cardHTML.style.top = currentTop + "px";
+    }
+  }
+}
+
+//need this for when I move a card from the discard...
+function renderDiscard() {
+  if (discards.length == 0) discardPile.innerHTML = "";
+  else discardPile.appendChild(discards[discards.length - 1].getHTML());
+}
+
+//make more efficient by creating a slide function that takes in the necesssary conditions
+//also move in to be inner functions
+function animateDeckReset() {
+  discardPile.innerHTML = "";
+
+  //adjusting z index bc discard card is on top of deck before it slides
+  discardPile.style.zIndex = "1";
+  deckPile.style.zIndex = "2";
+
+  let left = 110; //card width + column gap
+  let intervalID = setInterval(slide, 1);
+  deckPileDisplay.style.left = left + "px";
+  deckPile.appendChild(deckPileDisplay);
+
+  function slide() {
+    if (left < 0) clearInterval(intervalID);
+    else {
+      deckPileDisplay.style.left = left + "px";
+      left -= 1;
+    }
+  }
+}
+
+function animateDeckFlip() {
+  if (deck.length == 0) {
+    deckPile.innerHTML = "Click";
+  }
+  let discardCardHTML = discards[discards.length - 1].getHTML();
+
+  discardPile.style.zIndex = "2";
+  deckPile.style.zIndex = "1";
+
+  let left = -110;
+  let intervalID = setInterval(slide, 1);
+  discardCardHTML.style.left = left + "px";
+  discardPile.appendChild(discardCardHTML);
+
+  function slide() {
+    if (left > 0) clearInterval(intervalID);
+    else {
+      discardCardHTML.style.left = left + "px";
+      left += 1;
+    }
+  }
+}
+
+function animateCardFlip(card, cardDiv) {
+  let degrees = 0;
+  rotate();
+
+  function rotate() {
+    if (degrees > 360) return;
+    if (degrees == 90) {
+      degrees = 270;
+      card.faceUp = true;
+      card.justFlipped = false; //can i move this earlier?
+      let newCardDiv = card.getHTML();
+      newCardDiv.style.top = cardDiv.style.top;
+      newCardDiv.style.transform = "RotateY(" + degrees++ + "deg)";
+      cardDiv.parentNode.replaceChild(newCardDiv, cardDiv);
+      cardDiv = newCardDiv;
+    } else cardDiv.style.transform = "RotateY(" + degrees++ + "deg)";
+    setTimeout(() => rotate(), 1);
+  }
+}
+
+/*
+strange glitch where the bouding rect dimension are off by 1 px or so
+clean up code
+*/
